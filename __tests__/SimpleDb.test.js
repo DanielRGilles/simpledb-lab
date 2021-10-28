72af27537611de13f18aa53e56bfce2c56d44483
DanielRGilles <danryangilles@gmail.com>
2021-10-28 12:44:18 -0700

    worked on getAll, test still failing

--- __tests__/SimpleDb.test.js
+++ __tests__/SimpleDb.test.js
@@ -1,5 +1,6 @@
 const { mkdir, rm } = require('fs/promises');
 const SimpleDb = require('../lib/SimpleDb.js');
+const shortid = require('shortid');
 
 describe('SimpleDb', () => {
   const rootDir = './__tests__/store';
@@ -7,17 +8,36 @@ describe('SimpleDb', () => {
     return rm(rootDir, { force: true, recursive: true }).then(() => mkdir(rootDir, { recursive: true }));
   });
 
-  it('should save a json file in the store dir', () => {
-
-    const storeHouse = new SimpleDb(rootDir);
-    const jsonFile = { 'id': '1', 'name':'ruby!' } ;
+  it('should save a json file in the store dir and then retrieve', () => {
+    const shortyId = shortid.generate();
+    const storeHouse = new SimpleDb(rootDir, shortyId);
+    const jsonFile = { id: `${shortyId}`, name:'ruby!' } ;
 
     return storeHouse
       .store(jsonFile)
       .then(() => storeHouse.get())
-      .then((gotFile) => storeHouse.parse(gotFile))
       .then((retrievedFile) => expect(retrievedFile).toEqual(jsonFile));
   });
 
+  it('should return null for this one since nothing is being stored ', () => {
+    const shortyId = shortid.generate();
+    const storeHouse = new SimpleDb(rootDir, shortyId);
+    return storeHouse
+      .get()
+      .then((retrievedFile) => expect(retrievedFile).toEqual(null));
+
+  });
+  it('should return an array of objects that come from the files in the dir', () => {
+    const jsonFile = { id: `${shortid.generate()}`, name:'ruby!' } ;
+    const storeHouse = new SimpleDb(rootDir, jsonFile.id);
+
+    return storeHouse
+      .store(jsonFile)
+      .then(() => storeHouse.store(jsonFile))
+      .then(() => storeHouse.store(jsonFile))
+      .then(() => storeHouse.store(jsonFile))
+      .then(() => storeHouse.getAll())
+      .then((retrievedFile) => expect(retrievedFile).toEqual(jsonFile));
+  });
 
 });
