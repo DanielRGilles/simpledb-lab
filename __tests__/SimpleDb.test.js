b22f055026c3ff8fa792368009d43e54071daaa8
DanielRGilles <danryangilles@gmail.com>
2021-10-29 14:27:09 -0700

    reworked getAll and get(id) so tests were passing

--- __tests__/SimpleDb.test.js
+++ __tests__/SimpleDb.test.js
@@ -10,34 +10,38 @@ describe('SimpleDb', () => {
 
   it('should save a json file in the store dir and then retrieve', () => {
     const shortyId = shortid.generate();
-    const storeHouse = new SimpleDb(rootDir, shortyId);
+    const storeHouse = new SimpleDb(rootDir);
     const jsonFile = { id: `${shortyId}`, name:'ruby!' } ;
 
     return storeHouse
       .store(jsonFile)
-      .then(() => storeHouse.get())
+      .then(() => storeHouse.get(jsonFile.id))
       .then((retrievedFile) => expect(retrievedFile).toEqual(jsonFile));
   });
 
   it('should return null for this one since nothing is being stored ', () => {
-    const shortyId = shortid.generate();
-    const storeHouse = new SimpleDb(rootDir, shortyId);
+    const storeHouse = new SimpleDb(rootDir);
     return storeHouse
       .get()
       .then((retrievedFile) => expect(retrievedFile).toEqual(null));
 
   });
   it('should return an array of objects that come from the files in the dir', () => {
-    const jsonFile = { id: `${shortid.generate()}`, name:'ruby!' } ;
-    const storeHouse = new SimpleDb(rootDir, jsonFile.id);
-
+    const jsonFile = { name:'ruby!' } ;
+    const storeHouse = new SimpleDb(rootDir);
+    const expectation = [
+      { id: expect.any(String), name:'ruby!' },
+      { id: expect.any(String), name:'ruby!' },
+      { id: expect.any(String), name:'ruby!' },
+      { id: expect.any(String), name:'ruby!' }
+    ];
     return storeHouse
       .store(jsonFile)
       .then(() => storeHouse.store(jsonFile))
       .then(() => storeHouse.store(jsonFile))
       .then(() => storeHouse.store(jsonFile))
       .then(() => storeHouse.getAll())
-      .then((retrievedFile) => expect(retrievedFile).toEqual(jsonFile));
+      .then((retrievedArray) => expect(retrievedArray).toEqual(expectation));
   });
 
 });
